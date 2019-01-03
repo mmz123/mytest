e688318755528d37f870e667e6b4bb004556ded5
mmz123 <1163817801@qq.com>
2019-01-03 13:07:22 +0800

    commit and push

--- src/bit/edu/cn/knowledgeFinder.java
+++ src/bit/edu/cn/knowledgeFinder.java
@@ -3,8 +3,10 @@ package bit.edu.cn;
 public class knowledgeFinder {
 	public static void main(String[] args){
 		System.out.println("hello");
-		System.out.println("哎呦");
+		System.out.println("哎呦喂");
 		System.out.println("OK");
+		
+		
 		
 	}
 
